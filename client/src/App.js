198262b2a3e6da6a76756c7ac22570c1fd72aa2c
Keenan <116980114+KayoCodes@users.noreply.github.com>
2024-04-11 18:35:43 -0500

    Revert "HomePage"
    This reverts commit 330359beb6531a30698454557ab312ff395c25b2. Forgot to Make a Branch, This Commit should be safe to clone now

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./Navbar";
-import Navbar2 from "./Navbar2";
-import Promotion from "./Promotion";
-import FreqItems from "./FreqItems";
-import Wishlist from "./Wishlist";
-import Footer from "./Footer";
 
 function App() {
   const [backendData, setBackendData] = useState([{}]);
@@ -19,19 +13,11 @@ function App() {
 
   return (
     <div>
-      <Navbar/>
-      <Navbar2 />
-      <Promotion />
-      <FreqItems />
-      <Wishlist />
-      <Footer />
-
-      
-      {/* {typeof backendData.users === "undefined" ? (
+      {typeof backendData.users === "undefined" ? (
         <p>Loading...</p>
       ) : (
         backendData.users.map((user, i) => <p key={i}> {user}</p>)
-      )} */}
+      )}
     </div>
   );
 }
