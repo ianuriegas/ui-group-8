d8ee1e1d09793cacd6809aae32952148e39a950c
ianuriegas <75506938+ianuriegas@users.noreply.github.com>
2024-04-14 16:36:24 -0500

    created calls get product, discount and user information

--- server/server.js
+++ server/server.js
@@ -1,8 +1,51 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ "users": ["userOne", "userTwo", "userThree"] });
+const uri = "mongodb+srv://temp:temp1234@ui-group-8.migbrji.mongodb.net/";
+
+const client = new MongoClient(uri);
+
+app.get("/getProducts", async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db("group_8_db");
+    const products = database.collection("products");
+    const productsList = await products.find({}).toArray();
+    res.json(productsList);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get("/getDiscounts", async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db("group_8_db");
+    const products = database.collection("discounts");
+    const productsList = await products.find({}).toArray();
+    res.json(productsList);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get("/getUsers", async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db("group_8_db");
+    const products = database.collection("users");
+    const productsList = await products.find({}).toArray();
+    res.json(productsList);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  } finally {
+    await client.close();
+  }
 });
 
 app.listen(5001, () => {
