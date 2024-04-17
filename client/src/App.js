07131e014ad0eb31f6bc769aca31be312a2ab113
trinity-dennis <92657363+trinity-dennis@users.noreply.github.com>
2024-04-16 21:03:03 -0500

    added CategoryResults code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
 import Cart from './components/Cart';
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AccountPage from "./components/AccountPage";
 import { getCookie } from "./components/Navbar";
+import Category from "./components/Category";
+import ProductDetails from "./components/ProductDetails";
+import './App.css'
 
 function App() {
   const [username, setUsername] = React.useState('');
@@ -22,10 +24,11 @@ function App() {
   return (
       <Router>
         <Navbar />
-        
         <div className="content">
           <Routes>
             <Route exact path="/" element={<HomePage/>} />
+            <Route path="/category/:categoryName" element={<Category />} /> 
+            <Route path="/product/:productId" element={<ProductDetails />} />
             <Route path="/cart" element={<Cart/>} />
             {username ?
             <Route path="/account" element={<AccountPage name="Keenan Ray" address="323 Burgamy Way, Lands Between" cardnumber="4000-xxxx-xxxx-xxxx" expiredate="4/28"/>} />
