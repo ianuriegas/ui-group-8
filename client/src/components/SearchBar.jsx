e67f890319d4cb139142ab311678f6bf45cf7da4
ianuriegas <75506938+ianuriegas@users.noreply.github.com>
2024-04-16 21:12:24 -0500

    fixed navbar navigation to product pages.

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,18 +3,6 @@ import axios from 'axios';
 import { Autocomplete, InputAdornment, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-const categoryDisplayName = {
-    'Frozen': 'frozen',
-    'Dairy': 'dairy',
-    'Dry Foods': 'dry-foods',
-    'Bakery': 'bakery',
-    'Deli': 'deli',
-    'Produce': 'produce',
-    'Meat/Poultry': 'meat-poultry',
-    'Kitchenware': 'kitchenware',
-    'Home Essentials': 'home-essentials'
-}
-
 function SearchBar() {
     const [productData, setProductData] = React.useState([]);
     const navigate = useNavigate();
@@ -30,10 +18,7 @@ function SearchBar() {
 
     const handleSelect = (event, value) => {
         if (value && value.category) {
-            let category = categoryDisplayName[value.category];
-            if (category) {
-                navigate(`/${category}/${value._id}`);
-            }
+            navigate(`/product/${value._id}`);
         } else {
             console.error('Selected value is invalid:', value);
         }
