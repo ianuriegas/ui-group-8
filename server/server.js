78958280d384ad23b9f6ba0c0ba7be34546aa7f3
Angeline Milton <angelinemilton00@gmail.com>
2024-04-16 23:05:30 -0400

    updateServerJS

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const app = express();
 
 app.use(express.json());
 
-const uri = "mongodb+srv://temp:temp1234@ui-group-8.migbrji.mongodb.net/";
-const client = new MongoClient(uri);
+const uri = "mongodb+srv://temp:temp1234@ui-group-8.migbrji.mongodb.net/"
+let client;
+let db;
+
+const connectToMongo = async () => {
+  if (!client) {
+    client = new MongoClient(uri);
+      await client.connect();
+      db = client.db("group_8_db"); 
+      console.log("Connected to MongoDB");
+  }
+};
+
+connectToMongo().catch(console.error);
 
 //Function To Sort Items By Category
 function FetchCategory(itemArray, category){
@@ -30,61 +42,151 @@ function sortByPrice( itemArray){
 
 app.get("/getProducts", async (req, res) => {
   try {
-    await client.connect();
-    const database = client.db("group_8_db");
-    const products = database.collection("products");
+    const products = db.collection("products");
     
     const productsList = await products.find({}).toArray();
     res.json(productsList);
   } catch (e) {
     res.status(500).json({ error: e.message });
-  } finally {
-    await client.close();
   }
 });
 
+//Test Route
+// app.get("/getFrozen" , async (req, res) => {
+// try {
+//   await client.connect();
+//     const database = client.db("group_8_db");
+//     const products = database.collection("products");
+//     const productsList = await products.find({}).toArray();
+//     const frozenlist = await FetchCategory(productsList,"Dairy");
+    
+//     // frozenlist.sort((a,b) =>{
+//     //   return a.price - b.price;
+//     // });
+    
+//     console.log(sortByPrice(frozenlist));
+//     res.json(productsList);
+// } catch (e) {
+//   res.status(500).json({ error: e.message });
+  
+// }finally{
+//   await client.close();
+// }
+// });
+
 app.get("/getDiscounts", async (req, res) => {
   try {
-    await client.connect();
-    const database = client.db("group_8_db");
-    const products = database.collection("discounts");
+    const products = db.collection("discounts");
     const productsList = await products.find({}).toArray();
     res.json(productsList);
   } catch (e) {
     res.status(500).json({ error: e.message });
-  } finally {
-    await client.close();
   }
 });
 
 app.get("/getUsers", async (req, res) => {
   try {
-    await client.connect();
-    const database = client.db("group_8_db");
-    const products = database.collection("users");
-    const productsList = await products.find({}).toArray();
-    
-    res.json(productsList);
+    const users = db.collection("users");
+    const usersList = await users.find({}).toArray();
+    res.json(usersList);
   } catch (e) {
     res.status(500).json({ error: e.message });
-  } finally {
-    await client.close();
+  }
+});
+
+app.get("/getUserFromUsername", async (req, res) => {
+  try {
+    const { username } = req.query;
+    const users = db.collection("users");
+    const user = await users.findOne({ "username": username }); 
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 });
 
 app.post("/createUser", async (req, res) => {
   try {
-    await client.connect();
-    const database = client.db("group_8_db");
-    const users = database.collection("users");
-
-    // Insert the new user data from request body
+    const users = db.collection("users");
     const result = await users.insertOne(req.body);
     res.status(201).json(result);
   } catch (e) {
     res.status(500).json({ error: e.message });
-  } finally {
-    await client.close();
+  } 
+});
+
+app.post('/updateCart', async (req, res) => {
+  try {
+      const { username, productId, quantity } = req.body;
+
+      if (!username || !productId || typeof quantity !== 'number') {
+          return res.status(400).json({ error: 'Missing required fields or invalid data types.' });
+      }
+
+      const users = db.collection("users");
+
+      const user = await users.findOne({ username });
+
+      if (!user) {
+          return res.status(404).json({ error: 'User not found.' });
+      }
+
+      const itemExists = user.cart.items.find(item => item.productId === productId);
+
+      if (itemExists) {
+          await users.updateOne(
+              { username, "cart.items.productId": productId },
+              { $set: { "cart.items.$.quantity": quantity } }
+          );
+      } else {
+          await users.updateOne(
+              { username },
+              { $push: { "cart.items": { productId, quantity } } }
+          );
+      }
+
+      res.json({ message: 'Cart updated successfully.' });
+  } catch (error) {
+      console.error('Error updating the cart:', error);
+      res.status(500).json({ error: 'Failed to update cart.' });
+  }
+});
+
+app.put('/replaceCart', async (req, res) => {
+  try {
+      const { username, cart } = req.body;
+
+      // Validate the required inputs
+      if (!username || !cart || !Array.isArray(cart.items)) {
+          return res.status(400).json({ error: 'Missing required fields or invalid cart format.' });
+      }
+
+      const users = db.collection("users");
+
+      // Check if the user exists
+      const user = await users.findOne({ username });
+      if (!user) {
+          return res.status(404).json({ error: 'User not found.' });
+      }
+
+      // Replace the cart, ensuring that items and discountCode are correctly structured
+      const result = await users.updateOne(
+          { username },
+          { $set: { 'cart': { items: cart.items, discountCode: cart.discountCode || "" } } }
+      );
+
+      if (result.modifiedCount === 0) {
+          return res.status(404).json({ error: 'No changes made to the cart.' });
+      }
+
+      res.json({ message: 'Cart replaced successfully.' });
+  } catch (error) {
+      console.error('Error replacing cart:', error);
+      res.status(500).json({ error: 'Failed to replace cart.' });
   }
 });
 
@@ -96,9 +198,7 @@ app.post('/addToCart', async (req, res) => {
       return res.status(400).json({ error: 'Missing required fields.' });
     }
 
-    await client.connect();
-    const database = client.db("group_8_db");
-    const users = database.collection("users");
+    const users = db.collection("users");
 
     const user = await users.findOne({ username });
 
@@ -118,8 +218,6 @@ app.post('/addToCart', async (req, res) => {
   } catch (error) {
     console.error('Error adding product to cart:', error);
     res.status(500).json({ error: 'Failed to add product to cart.' });
-  } finally {
-    await client.close();
   }
 });
 
@@ -130,10 +228,7 @@ app.post('/addToSubscription', async (req, res) => {
     if (!username || !productId) {
       return res.status(400).json({ error: 'Missing required fields.' });
     }
-
-    await client.connect();
-    const database = client.db("group_8_db");
-    const users = database.collection("users");
+    const users = db.collection("users");
 
     const user = await users.findOne({ username });
 
@@ -153,8 +248,6 @@ app.post('/addToSubscription', async (req, res) => {
   } catch (error) {
     console.error('Error adding product to cart:', error);
     res.status(500).json({ error: 'Failed to add product to cart.' });
-  } finally {
-    await client.close();
   }
 });
 
@@ -166,9 +259,7 @@ app.post('/addToWishlist', async (req, res) => {
       return res.status(400).json({ error: 'Missing required fields.' });
     }
 
-    await client.connect();
-    const database = client.db("group_8_db");
-    const users = database.collection("users");
+    const users = db.collection("users");
 
     const user = await users.findOne({ username });
 
@@ -188,8 +279,6 @@ app.post('/addToWishlist', async (req, res) => {
   } catch (error) {
     console.error('Error adding product to cart:', error);
     res.status(500).json({ error: 'Failed to add product to cart.' });
-  } finally {
-    await client.close();
   }
 });
 
@@ -201,9 +290,7 @@ app.post('/addToFavorites', async (req, res) => {
       return res.status(400).json({ error: 'Missing required fields.' });
     }
 
-    await client.connect();
-    const database = client.db("group_8_db");
-    const users = database.collection("users");
+    const users = db.collection("users");
 
     const user = await users.findOne({ username });
 
@@ -223,8 +310,6 @@ app.post('/addToFavorites', async (req, res) => {
   } catch (error) {
     console.error('Error adding product to cart:', error);
     res.status(500).json({ error: 'Failed to add product to cart.' });
-  } finally {
-    await client.close();
   }
 });
 
@@ -236,9 +321,7 @@ app.post('/removeFromFavorites', async (req, res) => {
       return res.status(400).json({ error: 'Missing required fields.' });
     }
 
-    await client.connect();
-    const database = client.db("group_8_db");
-    const users = database.collection("users");
+    const users = db.collection("users");
 
     const user = await users.findOne({ username });
 
@@ -258,11 +341,8 @@ app.post('/removeFromFavorites', async (req, res) => {
   } catch (error) {
     console.error('Error adding product to cart:', error);
     res.status(500).json({ error: 'Failed to add product to cart.' });
-  } finally {
-    await client.close();
   }
 });
-
 
 app.listen(5001, () => {
   console.log("Server started on port 5001");
